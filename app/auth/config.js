80499453a06ddd3684fa817db01196a43fbfb0fb
ashwinhingve <140718874+ashwinhingve@users.noreply.github.com>
2025-02-05 18:01:54 +0530

    Update config.js
    add api route

--- app/auth/config.js
+++ app/auth/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: 'https://dec-azure.vercel.app/auth_api.php'  // Replace with your actual domain
+  API_URL: 'https://dec-azure.vercel.app/api/auth_api.php'  // Replace with your actual domain
 };
 
 export default config;
