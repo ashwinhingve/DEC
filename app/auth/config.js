f4e656e79a2182f430d61d1fd59c56974bc17b6f
ashwinhingve <140718874+ashwinhingve@users.noreply.github.com>
2025-02-05 18:26:54 +0530

    Update config.js
    change domain url

--- app/auth/config.js
+++ app/auth/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: 'https://dec-azure.vercel.app/api/auth_api.php'  // Replace with your actual domain
+  API_URL: 'https://demploymentcorner.com/api/auth_api.php'  // Replace with your actual domain
 };
 
 export default config;
