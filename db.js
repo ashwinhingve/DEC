98a508944c67e790821e04a90dc633244b549e6b
ashwinhingve <ashwin.hingave123@gmail.com>
2025-02-06 17:50:04 +0530

    commit message

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 const db = mysql.createPool({
   host: "srv1740.hstgr.io",
   user: "u436222305_auth_db", // Default XAMPP user
-  password: "Demploymentcorner@123", // Default XAMPP password (leave empty)
+  password: "Demploymentcorner@123", 
   database: "u436222305_auth_db",
 });
 
